703e4e4300011aa42d84035b033e77b00ffb0844
Mudasira Sultana <mudasirasultana@Mudasiras-MacBook-Air.local>
2025-11-20 14:45:40 +0530

    Configure Hardhat for Render (0.0.0.0 host) and add start script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
+// require("@nomicfoundation/hardhat-toolbox");
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.28",
+// };
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.19",
+  networks: {
+    hardhat: {
+      // allow the node to bind to all interfaces on Render
+      host: "0.0.0.0",
+      port: 8545,
+      chainId: 31337,
+    }
+  }
 };
