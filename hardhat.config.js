d6462b49898d34b17e1f2a81f46ea2eb5ddb69c0
Mudasira Sultana <mudasirasultana@Mudasiras-MacBook-Air.local>
2025-11-20 15:08:05 +0530

    Added multi-solidity compiler versions

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,13 +7,26 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    compilers: [
+      { version: "0.8.19" },
+      { version: "0.8.28" }
+    ]
+  },
+
   networks: {
     hardhat: {
-      // allow the node to bind to all interfaces on Render
       host: "0.0.0.0",
       port: 8545,
       chainId: 31337,
+    },
+
+    render: {
+      url: "https://sentinel-chain-3.onrender.com",
+      chainId: 31337,
+      accounts: [
+        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+      ],
     }
   }
 };
